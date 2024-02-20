92b45eb7c6c59697cd36592f6271e3771c78e9ff
Himanshu Singh <himanshuab9@gmail.com>
2024-02-20 16:51:08 +0530

    All setup configured

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 // import mongoose from "mongoose";
 // import { DB_NAME } from "./constants";
 dotenv.config({
